43b107f3db1f8eaa4f3cd77d03065f504f380648
russhooper <russ.hooper.24@gmail.com>
2024-07-20 18:09:59 -0400

    Initial Commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Russ Hooper on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
